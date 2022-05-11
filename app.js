7c9fcd14cfb6f15eb7f0c459115e28c7feefb49d
K Sankalp Patnaik <sankalppatnaik143@gmail.com>
2022-05-11 18:37:56 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+const request=require("request");
+const https=require("https");
+
+
+//Here We using a Method from express:- app.use(express.static("name of the folder here Name of the folder is Public"))
+//In Public Folder stored local files like css folder, Images folder 
+//inside css folder styles.css file is there & inside images folder logo image is there,
+//which are stored in locally so thats why we using express.static method to send those files to Local host 3000
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/",function(req,res){
+    const firstName=req.body.fname;
+    const lastname=req.body.lname;
+    const Email=req.body.email;
+
+    const data={
+        members:
+        [                   //Members are array of objects
+            {
+                email_address:Email,
+                status:"subscribed",
+                merge_field:{ 
+                    FNAME:firstName, 
+                    LNAME:lastname
+                }
+            }
+        ]
+    };
+    //converting the data into String that is in the the format JSON
+    const Jsondata=JSON.stringify(data);
+    const url="https://us10.api.mailchimp.com/3.0/lists/ff1fb09624"
+    const options={
+        method:"POST",
+        auth:"sankalp1:ab5136a35660d3c38c9c49f3e3f59b3f-us10"
+    }
+   const request= https.request(url,options,function(response){
+       if(response.statusCode===200)
+        {
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+           res.sendFile(__dirname+"/failure.html")
+        }
+       response.on("data",function(data){
+           console.log(JSON.parse(data));
+           
+       })
+
+    })
+    request.write(Jsondata);
+    request.end();
+});
+app.post("/failure",function(request,response){
+   response.redirect("/");
+});
+app.post("/success",function(req,res){
+    res.redirect("/");
+})
+// app.post("/success",function(req,res){
+//     res.redirect("/");
+// })
+app.listen(process.env.PORT||3000,function()
+{
+    console.log("Server Is Running on Port 3000");
+})
+//ApI Key:- ab5136a35660d3c38c9c49f3e3f59b3f-us10
+
+//List Id or Audience Id:- ff1fb09624
+
+//${dc}
